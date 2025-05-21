cfee76553d72b6b2be1bfa68c50d977d19b16f69
printMostafa <160338399+printMostafa@users.noreply.github.com>
2025-05-21 19:22:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from surprise import Dataset, Reader, SVD
